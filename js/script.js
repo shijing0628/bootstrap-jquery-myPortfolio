586b83e0095459900dbd39e6565770f0636c2467
Shijing Liu <brendaliu0628@gmail.com>
2020-10-11 15:20:03 -0500

    gallery page was completed

--- js/script.js
+++ js/script.js
@@ -3,14 +3,34 @@
    /* ---------------------------------------------- */
 
 $(window).on('load', function () {
- $('#preloader').addClass("loaded");
+   $('#preloader').addClass("loaded");
 });
 
 
 // navbar
 $('.navbar-toggler').on('click', function () {
- $('body').toggleClass('aside-open');
- $('.ham').toggleClass('active');
- $('body,html').toggleClass('overflow-hidden');
+   $('body').toggleClass('aside-open');
+   $('.ham').toggleClass('active');
+   $('body,html').toggleClass('overflow-hidden');
 })
 
+
+// gallery filter
+
+var $portfolioContainer = $('.list-items-container');
+$('#filter li').on('click', function (e) {
+   e.preventDefault();
+   $('#filter li').removeClass('active');
+   $(this).addClass('active');
+   var groupName = $(this).attr('data-group');
+   $portfolioContainer.shuffle('shuffle', groupName);
+});
+// slider
+
+var swipertest = new Swiper('.swiper-testimony', {
+   spaceBetween: 30,
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+});
